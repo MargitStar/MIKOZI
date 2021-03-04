1db238e5041d301b7723013e69f3498030267e29
margitstar <sovenokbymargo@mail.ru>
2021-03-05 00:34:28 +0300

    Add 1st task Affine

--- lab1.py
+++ lab1.py
@@ -1,14 +1,74 @@
+import math
+
 alphabet = list('АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ')
+
+# Task 1
+
+# int gcd (int a, int b, int & x, int & y) {
+#   if (a == 0) {
+#     x = 0; y = 1;
+#     return b;
+#   }
+#   int x1, y1;
+#   int d = gcd (b%a, a, x1, y1);
+#   x = y1 - (b / a) * x1;
+#   y = x1;
+#   return d;
+# }
+
+def gcd(a, b):
+    if not b:
+        return (1, 0, a)
+    y, x, g = gcd(b, a % b)
+    return (x, y - (a // b) * x, g)
+
+
+def affine_encrypt(text, alphabet, a, b):
+    m = len(alphabet)
+
+    encrypted = []
+
+    for char in text:
+        encrypted.append(alphabet[((a * alphabet.index(char)) + b) % m])
+    return ''.join(encrypted)
+
+
+def affine_decrypt(text, alphabet, a, b):
+    m = len(alphabet)
+
+    decrypted = []
+
+    x, y = 0, 0
+    x, y, g = gcd(a, m)
+    if g != 1:
+        print("Error")
+    else:
+        x = (x % m + m) % m
+
+    for char in text:
+        pos = (x * (alphabet.index(char) - b)) % m
+        pos += m if pos < 0 else 0
+        decrypted.append(alphabet[pos])
+
+    return "".join(decrypted)
+
+print(f'Initial text: БОБКОВА')
+
+
+affine_en = affine_encrypt('БОБКОВА', alphabet, 8, 18)
+print("Encrypted:", affine_en)
+
+print("Decrypted: ",affine_decrypt(affine_en, alphabet, 8, 18))
 
 
 # Task 2 (Vigenère cipher)
-
 key = list('ДУЦ')
 text = list('ЬЬКФЁЫОЕИ')
 
 
-def vigenere_cipher_encrypt(alphabet, key, text):
-    new_text_indexes = [(alphabet.index(value) + alphabet.index(key[index % len(key)])) % len(alphabet) for index, value in enumerate(text)]
+def vigenere_cipher_decrypt(alphabet, key, text):
+    new_text_indexes = [(alphabet.index(value) + alphabet.index(key[index % len(key)])) %
+                        len(alphabet) for index, value in enumerate(text)]
 
     encoded = ''
 
@@ -17,8 +77,10 @@ def vigenere_cipher_encrypt(alphabet, key, text):
 
     return encoded
 
-def vigenere_cipher_decrypt(alphabet, key, text):
-    new_text_indexes = [alphabet.index(value) - alphabet.index(key[index % len(key)]) % len(alphabet) for index, value in enumerate(text)]
+
+def vigenere_cipher_encrypt(alphabet, key, text):
+    new_text_indexes = [alphabet.index(value) - alphabet.index(
+        key[index % len(key)]) % len(alphabet) for index, value in enumerate(text)]
 
     decoded = ''
 
@@ -27,13 +89,12 @@ def vigenere_cipher_decrypt(alphabet, key, text):
 
     return decoded
 
+
 initial_text = ''.join(text)
-print(f'Initial text: {str(initial_text)}')
+print(f'\nInitial text: {str(initial_text)}')
 
-decoded = vigenere_cipher_decrypt(alphabet, key, text)
-print(f'Decrypted: {decoded}')
+decoded = vigenere_cipher_encrypt(alphabet, key, text)
+print(f'Encrypted: {decoded}')
 
-encoded = vigenere_cipher_encrypt(alphabet, key, decoded)
-print(f'Encrypted: {encoded}')
-
-
+encoded = vigenere_cipher_decrypt(alphabet, key, decoded)
+print(f'Decrypted: {encoded}')
